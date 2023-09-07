97f4083c8bf0d42f86f8eb0f1c0c9c9b1d6ebf5f
saiful islam <saifulislam975317@gmail.com>
2023-09-07 11:11:07 +0600

    update info

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
@@ -29,6 +29,7 @@ async function run() {
     const watchesCollection = client.db("watchShopDb").collection("watches");
     const reviewCollection = client.db("watchShopDb").collection("reviews");
     const cartsCollection = client.db("watchShopDb").collection("carts");
+    const usersCollection = client.db("watchShopDb").collection("users");
 
     // api routes
     app.get("/watchData", async (req, res) => {
@@ -44,8 +45,41 @@ async function run() {
 
     app.post("/carts", async (req, res) => {
       const newItem = req.body;
-      console.log(newItem);
+
       const result = await cartsCollection.insertOne(newItem);
+      res.send(result);
+    });
+
+    app.get("/carts/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // users api
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exit" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find({}).toArray();
       res.send(result);
     });
 
